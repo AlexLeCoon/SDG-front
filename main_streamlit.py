a33cbf2c3f4ac1956a150d5100dc14d33ae3a4c0
alexandre le saux <alexandre.lesaux.98@gmail.com>
2023-09-06 15:13:59 +0200

    add pdf modif streamlit

--- main_streamlit.py
+++ main_streamlit.py
@@ -11,56 +11,56 @@ st.title("Using a PDF :open_file_folder:")
 with st.form(key='params_for_api_pdf'):
     uploaded_file = st.file_uploader("Choose a file", type= 'pdf')
     if st.form_submit_button("Upload pdf"):
-        st.form_submit_button("which SDG am I?")
-        if uploaded_file is not None:
-            pdf=PDFQuery(uploaded_file)
-            pdf.load()
-            text_elements=pdf.pq("LTTextLineHorizontal")
-            text=[t.text for t in text_elements]
-            #text = pdf(uploaded_file)
-            text = " ".join(text)
-            params = {'text':text}
-            sdg_classifier_api_url = f"https://sdgclassifier-bw4yive63a-od.a.run.app/predict"
-            response = requests.get(sdg_classifier_api_url,params=params)
-            prediction = response.json()
+        if st.form_submit_button("which SDG am I?"):
 
-            pred = prediction['The text is talking about SDG:']
+            if uploaded_file is not None:
+                pdf=PDFQuery(uploaded_file)
+                pdf.load()
+                text_elements=pdf.pq("LTTextLineHorizontal")
+                text=[t.text for t in text_elements]
+                text = " ".join(text)
+                params = {'text':text}
+                sdg_classifier_api_url = f"https://sdgclassifier-bw4yive63a-od.a.run.app/predict"
+                response = requests.get(sdg_classifier_api_url,params=params)
+                prediction = response.json()
 
-            if round(pred) == 15:
-                st.image("https://miro.medium.com/v2/resize:fit:1400/format:webp/1*7MDLuoSaJjS-q5tZ_vJbVA.png")
+                pred = prediction['The text is talking about SDG:']
 
-            elif round(pred) == 1:
-                st.image("https://miro.medium.com/v2/resize:fit:1400/format:webp/1*9KeYUomO4E0EqXT24XUypQ.png")
+                if round(pred) == 15:
+                    st.image("https://miro.medium.com/v2/resize:fit:1400/format:webp/1*7MDLuoSaJjS-q5tZ_vJbVA.png")
 
-            elif round(pred) == 2:
-                st.image("https://www.kit.nl/wp-content/uploads/2019/02/E_SDG-goals_icons-individual-rgb-02.png")
+                elif round(pred) == 1:
+                    st.image("https://miro.medium.com/v2/resize:fit:1400/format:webp/1*9KeYUomO4E0EqXT24XUypQ.png")
 
-            elif round(pred) == 3:
-                st.image("https://miro.medium.com/v2/resize:fit:1400/format:webp/1*bD6Q8IDG3Ef444SAOnNiyg.png")
-            elif round(pred) == 4:
-                st.image("https://miro.medium.com/v2/resize:fit:1400/format:webp/1*1-A2Y3EWTX6V8ISs7zgU_Q.png")
-            elif round(pred) == 5:
-                st.image("https://www.kit.nl/wp-content/uploads/2019/02/E_SDG-goals_icons-individual-rgb-05.png")
-            elif round(pred) == 6:
-                st.image("https://www.kit.nl/wp-content/uploads/2019/02/E_SDG-goals_icons-individual-rgb-06.png")
-            elif round(pred) == 7:
-                st.image("https://www.kit.nl/wp-content/uploads/2019/02/E_SDG-goals_icons-individual-rgb-07.png")
-            elif round(pred) == 8:
-                st.image("https://www.kit.nl/wp-content/uploads/2019/02/E_SDG-goals_icons-individual-rgb-08.png")
-            elif round(pred) == 9:
-                st.image("https://www.kit.nl/wp-content/uploads/2019/02/E_SDG-goals_icons-individual-rgb-09.png")
-            elif round(pred) == 10:
-                st.image("https://www.kit.nl/wp-content/uploads/2019/02/E_SDG-goals_icons-individual-rgb-10.png")
-            elif round(pred) == 11:
-                st.image("https://www.kit.nl/wp-content/uploads/2019/02/E_SDG-goals_icons-individual-rgb-11.png")
-            elif round(pred) == 12:
-                st.image("https://www.kit.nl/wp-content/uploads/2019/02/E_SDG-goals_icons-individual-rgb-12.png")
-            elif round(pred) == 13:
-                st.image("https://www.kit.nl/wp-content/uploads/2019/02/E_SDG-goals_icons-individual-rgb-13.png")
-            elif round(pred) == 14:
-                st.image("https://www.kit.nl/wp-content/uploads/2019/02/E_SDG-goals_icons-individual-rgb-14.png")
-            elif round(pred) == 16:
-                st.image("https://www.kit.nl/wp-content/uploads/2019/02/E_SDG-goals_icons-individual-rgb-16.png ")
+                elif round(pred) == 2:
+                    st.image("https://www.kit.nl/wp-content/uploads/2019/02/E_SDG-goals_icons-individual-rgb-02.png")
+
+                elif round(pred) == 3:
+                    st.image("https://miro.medium.com/v2/resize:fit:1400/format:webp/1*bD6Q8IDG3Ef444SAOnNiyg.png")
+                elif round(pred) == 4:
+                    st.image("https://miro.medium.com/v2/resize:fit:1400/format:webp/1*1-A2Y3EWTX6V8ISs7zgU_Q.png")
+                elif round(pred) == 5:
+                    st.image("https://www.kit.nl/wp-content/uploads/2019/02/E_SDG-goals_icons-individual-rgb-05.png")
+                elif round(pred) == 6:
+                    st.image("https://www.kit.nl/wp-content/uploads/2019/02/E_SDG-goals_icons-individual-rgb-06.png")
+                elif round(pred) == 7:
+                    st.image("https://www.kit.nl/wp-content/uploads/2019/02/E_SDG-goals_icons-individual-rgb-07.png")
+                elif round(pred) == 8:
+                    st.image("https://www.kit.nl/wp-content/uploads/2019/02/E_SDG-goals_icons-individual-rgb-08.png")
+                elif round(pred) == 9:
+                    st.image("https://www.kit.nl/wp-content/uploads/2019/02/E_SDG-goals_icons-individual-rgb-09.png")
+                elif round(pred) == 10:
+                    st.image("https://www.kit.nl/wp-content/uploads/2019/02/E_SDG-goals_icons-individual-rgb-10.png")
+                elif round(pred) == 11:
+                    st.image("https://www.kit.nl/wp-content/uploads/2019/02/E_SDG-goals_icons-individual-rgb-11.png")
+                elif round(pred) == 12:
+                    st.image("https://www.kit.nl/wp-content/uploads/2019/02/E_SDG-goals_icons-individual-rgb-12.png")
+                elif round(pred) == 13:
+                    st.image("https://www.kit.nl/wp-content/uploads/2019/02/E_SDG-goals_icons-individual-rgb-13.png")
+                elif round(pred) == 14:
+                    st.image("https://www.kit.nl/wp-content/uploads/2019/02/E_SDG-goals_icons-individual-rgb-14.png")
+                elif round(pred) == 16:
+                    st.image("https://www.kit.nl/wp-content/uploads/2019/02/E_SDG-goals_icons-individual-rgb-16.png ")
 
 st.header("",divider="rainbow")
 
